dde7dd5a0e97fbaa2ecee04a68030baf578773fc
Pablo Carli <pablodcarli@gmail.com>
2023-05-04 17:41:05 -0300

    Update with bootstrap and node modules

---
+++ mugshot.svelte
@@ -0,0 +1,48 @@
+<script>
+import { onMount } from "svelte";
+
+export default {
+  name: "ZoomableImage",
+  props: {
+    src: String,
+    title: String,
+  },
+  onMount() {
+    const img = this.$refs.img;
+    img.addEventListener("mouseover", () => {
+      img.style.transform = "scale(1.2)";
+    });
+    img.addEventListener("mouseleave", () => {
+      img.style.transform = "scale(1)";
+    });
+  },
+  render() {
+    return (
+      <div>
+        <img
+          ref="img"
+          src={this.src}
+          alt={this.title}
+        />
+        <div class="title">{this.title}</div>
+      </div>
+    );
+  },
+};
+</script>
+
+<style>
+.title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 20px;
+}
+</style>
+
+<ZoomableImage
+  src="/img/a-man-coding.jpg"
+  title="Hombre Codeando"
+/>
